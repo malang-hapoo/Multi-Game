6f7c1487b268bc3803322ef1d4cf3456e6120898
malang-hapoo <mukkang1210@gmail.com>
2025-11-10 12:35:08 +0900

    홈화면 추가

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // 메뉴 스크롤
 let currentIndex = 0;
 
-function showContent(index) {
+function showContentDelay(index,second) {
     const wrap = document.querySelector('.main-wrap');
     const menus = document.querySelectorAll('.submenu');
 
-    // 이동 거리 계산
     const distance = Math.abs(index - currentIndex);
 
-    // 거리 비례 트랜지션 시간 (1초 + 0.5 × 거리)
     const duration = 1 + 0.5 * (distance - 1);
-
-    // transition 시간 동적으로 설정
     wrap.style.transition = `${duration}s`;
 
-    // 슬라이드 이동
-    wrap.style.transform = `translateX(-${index * 100}%)`;
+    const delay = 1000 * second;
 
-    // 메뉴 active 상태 표시
-    menus.forEach(menu => menu.classList.remove('active'));
-    menus[index].classList.add('active');
+    setTimeout(function() {
+        // 슬라이드 이동
+        wrap.style.transform = `translateX(-${index * 100}%)`;
 
-    // 현재 인덱스 갱신
-    currentIndex = index;
+        // 메뉴 active 상태 표시
+        menus.forEach(menu => menu.classList.remove('active'));
+        menus[index].classList.add('active');
+
+        // 현재 인덱스 갱신
+        currentIndex = index;
+    },delay)
 }
 
 //학과 스크롤
@@ -41,4 +41,13 @@ function scrollDepartment(index) {
 
     // 현재 인덱스 갱신
     scrolltIndex = index;
+}
+
+//원하는 위치 세로 스크롤
+
+function scrollDistanceDelay(distance,second) {
+    const delay = 1000 * second;
+    setTimeout(function() {
+        window.scrollTo({top: distance, behavior:'smooth'})
+    }, delay)
 }
